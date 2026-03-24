94f6a7925293bceb75fef6989fe1bc430f14e482
unknown <911991sudhir@gmail.com>
2026-03-24 16:00:12 +0530

    added

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -6,62 +6,30 @@ import { ApiService } from '../../core/services/api.service';
 import { Property } from '../../core/models/property.model';
 import { PropertyCardComponent } from '../../shared/property-card/property-card.component';
 import { SkeletonLoaderComponent } from '../../shared/skeleton-loader/skeleton-loader.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule, PropertyCardComponent, SkeletonLoaderComponent],
+  imports: [CommonModule, RouterLink, FormsModule, PropertyCardComponent, SkeletonLoaderComponent, CarouselModule],
   template: `
-   <!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Example headline.</h1>
-              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>One more for good measure.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div><!-- /.carousel -->
+   <carousel>
+  <slide>
+    <a href="#" target="_blank"><img src="assets/images/carousel/1.jpg" alt="first slide" style="display: block; width: 100%;"></a>
+  </slide>
+  <slide>
+    <a href="#" target="_blank"><img src="assets/images/carousel/2.jpg" alt="second slide" style="display: block; width: 100%;"></a>
+  </slide>
+  <slide>
+    <a href="#" target="_blank"><img src="assets/images/carousel/3.jpg" alt="third slide" style="display: block; width: 100%;"></a>
+  </slide>
+</carousel>
+<br/>
+<div>
+  <button type="button" class="btn btn-info"
+    (click)="switchIndicator()">Enable/Disable Indicator
+  </button>
+</div>
 
     <div class="loancalcicon" id="loancalcid" onClick="showpopup1()">
 	<img src="assets/images/calcicon.jpg">
@@ -864,9 +832,20 @@ export class HomeComponent implements OnInit {
   loading = true;
   searchType = 'buy';
   searchQuery = '';
-  
+
 
   constructor(private api: ApiService, private router: Router, private cdr: ChangeDetectorRef) {}
+
+   slides: {image: string; text?: string}[] =  [
+    {image: 'assets/images/nature/5.jpg'},
+    {image: 'assets/images/nature/4.jpg'},
+    {image: 'assets/images/nature/3.jpg'}
+  ];
+  showIndicator = true;
+ 
+  switchIndicator(): void {
+    this.showIndicator = !this.showIndicator;
+  }
 
   ngOnInit() {
     
